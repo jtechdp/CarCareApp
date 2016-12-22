d383049bb11cca63a03e48aa0d5981a412e2e213
Evgen Mulevich <jtechdp@ukr.net>
2016-12-23 00:37:31 +0200

    CarCareApp web site v.0.6.1

---
+++ text.js
@@ -0,0 +1,27 @@
+
+    var e = document.getElementById("submitForm");
+
+    e.onclick = function () {
+    	 
+	      var xhr = new XMLHttpRequest();
+
+	      xhr.open('GET', 'fail.json', false);
+	      xhr.send();
+
+
+		if (xhr.status != 200) {
+		// если ошибка то обработать ошибку
+		document.getElementById('format').innerHTML = 'Ошибка ' + xhr.status + ': ' + xhr.statusText;
+		} else {
+		// если все ровно то вывести результат
+		var textRes = xhr.responseText;
+		textRes = JSON.parse(textRes);
+
+		document.getElementById('format').innerHTML = '<br/><br/><ul><li>' + textRes.name + '</li><li>' +
+			textRes.surname + '</li><li>' + textRes.tel + '</li><li>' + textRes.email + '</li><li>' +
+			textRes.password + '</li><li>' + textRes.stringST + '</li><li>' + textRes.age + '</li><li>' 
+			+ textRes.xz + '</li>';
+			
+		}
+
+    }
